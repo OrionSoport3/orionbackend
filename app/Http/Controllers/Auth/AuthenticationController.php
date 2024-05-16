557d103eed5ef818129d26b6a267791b0364d290
eddca <edd.cazj0420@gmail.com>
2024-05-16 14:01:17 -0600

    primer commit

---
+++ app/Http/Controllers/Auth/AuthenticationController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\RegisterRequest;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthenticationController extends Controller
+{
+    public function register(RegisterRequest $request) {
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        $token = JWTAuth::fromUser($user);
+
+        // return response()->json([
+        //     'message' => 'Registro exitoso'
+        // ],200);
+
+        return response()->json(compact('user', 'token'), 201);
+    }
+
+    public function login(LoginRequest $request) {
+        $credentials = $request->only('email', 'password');
+
+        if(!$token = JWTAuth::attempt($credentials)){
+            return response()->json(['error' => 'invalid_credentials'], 401);
+        }
+
+        $user = User::where('email',$request->email)->first();
+
+        return response()->json(compact('user', 'token'), 200);
+    }
+}
